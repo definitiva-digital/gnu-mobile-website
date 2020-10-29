3fdaeb4388e4fe155bd9f014d3ab384036e9c3a5
definitiva-digital <raphael@definitivadigital.com.br>
2020-10-29 17:13:45 -0300

    Insert footer

--- public_html/views/theme/footer.php
+++ public_html/views/theme/footer.php
@@ -1,4 +1,4 @@
-<section class="large content padding-5-10 center middle">
+<section class="large content center middle padding-5-10">
     <article class="content column column-2 padding-hor-5">
         <h3 class="text-primary size-26 margin-bottom">Comece agora mesmo</h3>
         <p class="size18 line-2">1. Preencha os dados ao lado</p>
@@ -16,10 +16,8 @@
 </section>
 
 <footer>
-    <section class="footer-logo large">
-        <div>
-            <img src="<?= asset('images/gnu-complete-logo-white.png') ?>">
-        </div>
+    <section class="footer-logo">
+        <img src="<?= asset('images/gnu-complete-logo.png') ?>">
     </section>
     <section class="content large">
         <div class="content column column-4 text-light">
@@ -39,6 +37,58 @@
                 </a>
             </article>
         </div>
+        <div class="content column column-4 text-light">
+            <article class="footer-content">
+                <h4>Serviços</h4>
+                <a class="text-light" href="" target="_blank">
+                    <i class="fa fa-envelope"></i>SMS Corporativo
+                </a>
+                <a class="text-light" href="" target="_blank">
+                    <i class="fa fa-whatsapp"></i>SMS Interativo
+                </a>
+                <a class="text-light" href="" target="_blank">
+                    <i class="fa fa-ban"></i>SMS Marketing
+                </a>
+                <a class="text-light" href="mailto:contato@gnumobile.com.br" target="_blank">
+                    <i class="fa fa-file"></i>Relatórios
+                </a>
+            </article>
+        </div>
+        <div class="content column column-4 text-light">
+            <article class="footer-content">
+                <h4>Desenvolvedores</h4>
+                <a class="text-light" href="" target="_blank">
+                    <i class="fa fa-envelope"></i>Documentação api
+                </a>
+                <a class="text-light" href="" target="_blank">
+                    <i class="fa fa-whatsapp"></i>Integrações
+                </a>
+            </article>
+        </div>
+        <div class="content column column-4 text-light">
+            <article class="footer-content">
+                <h4>Conteúdos</h4>
+                <a class="text-light" href="" target="_blank">
+                    <i class="fa fa-envelope"></i>blog
+                </a>
+                <a class="text-light" href="" target="_blank">
+                    <i class="fa fa-whatsapp"></i>E-book
+                </a>
+                <h4 class="margin-top-5">Formas de pagamento</h4>
+                <div class="large content jus-sbetween" id="footer-payment-icons">
+                        <i class="fa fa-credit-card"></i>
+                        <i class="fa fa-credit-card-alt"></i>
+                        <i class="fa fa-credit-card"></i>
+                        <i class="fa fa-credit-card-alt"></i>
+                </div>
+            </article>
+        </div>
+    </section>
+    <section class="large padding-ver margin-top thin">
+        <article class="content jus-sbetween middle text-light text-center">
+                <span>C 2020 Copyright - GNU MOBILE</span>
+                <span>Desenvolvido por <a href="https://definitivadigital.com.br" target="_blank">Definitiva Digital</a></span>
+        </article>
     </section>
 </footer>
 
